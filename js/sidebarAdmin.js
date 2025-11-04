b16433bea13070c2745a1a3cacaa6dd03dcb217d
huong2004 <22130095@st.hcmuaf.edu.vn>
2025-11-04 09:54:04 +0700

    cập nhật trang quản lí sản phẩm

--- js/sidebarAdmin.js
+++ js/sidebarAdmin.js
@@ -13,29 +13,29 @@ document.addEventListener("click", function(e) {
 });
 
 // Load sidebarAdmin cho các trang của admin
-(async function loadSidebar() {
-    const container = document.getElementById("sidebarContainer");
-    if (!container) return;
-
-    try {
-        const res = await fetch("sideBarAdmin.html");
-        if (!res.ok) throw new Error("Không thể load Sidebar");
-
-        const html = await res.text();
-        const doc = new DOMParser().parseFromString(html, "text/html");
-        const aside = doc.querySelector("aside");
-
-        if (!aside) return console.error("Không tìm thấy thẻ <aside> trong sidebar");
-
-        container.replaceWith(aside);
-        // gọi hàm đóng mở khi load sidebar
-        toggleSidebar();
-        activeSidebar();
-
-    } catch (err) {
-        console.error(err);
-    }
-})();
+// (async function loadSidebar() {
+//     const container = document.getElementById("sidebarContainer");
+//     if (!container) return;
+//
+//     try {
+//         const res = await fetch("sideBarAdmin.html");
+//         if (!res.ok) throw new Error("Không thể load Sidebar");
+//
+//         const html = await res.text();
+//         const doc = new DOMParser().parseFromString(html, "text/html");
+//         const aside = doc.querySelector("aside");
+//
+//         if (!aside) return console.error("Không tìm thấy thẻ <aside> trong sidebar");
+//
+//         container.replaceWith(aside);
+//         // gọi hàm đóng mở khi load sidebar
+//         toggleSidebar();
+//         activeSidebar();
+//
+//     } catch (err) {
+//         console.error(err);
+//     }
+// })();
 
 // đóng mở menu
 function toggleSidebar() {
