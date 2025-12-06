860e3995c8d025fe4fcec40353659560bd02d053
huong2004 <22130095@st.hcmuaf.edu.vn>
2025-12-06 22:49:24 +0700

    Remove WAR file from tracking

---
+++ WebDynamic/src/main/java/vn/edu/hcmuaf/fit/webdynamic/controller/user/LoginServlet.java
@@ -0,0 +1,9 @@
+package vn.edu.hcmuaf.fit.webdynamic.controller.user;
+
+import jakarta.servlet.annotation.WebServlet;
+
+@WebServlet(name = "LoginServlet", value = "/login")
+public class LoginServlet {
+
+
+}
