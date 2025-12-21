f446d9e33bfe20eb5239a3a396a572d6a1d2ec80
huong2004 <22130095@st.hcmuaf.edu.vn>
2025-12-21 22:27:02 +0700

    create trang feedback

--- WebDynamic/src/main/java/vn/edu/hcmuaf/fit/webdynamic/service/UserService.java
+++ WebDynamic/src/main/java/vn/edu/hcmuaf/fit/webdynamic/service/UserService.java
@@ -10,28 +10,30 @@ import java.util.Optional;
 public class UserService {
     private final UserInfoDao userDao;
 
-    private UserDao userDao = UserDao.getInstance();
+    private UserDao userDAO = UserDao.getInstance();
 
     // Đăng ký (hash password)
     public boolean register(User user) {
         String hash = BCrypt.hashpw(user.getPassword(), BCrypt.gensalt());
         user.setPassword(hash);
-        return userDao.register(user);
+        return userDAO.register(user);
     }
 
     // Đăng nhập (check BCrypt)
     public User login(String input, String password) {
-        User user = userDao.findByInput(input);
+        User user = userDAO.findByInput(input);
         if (user == null) return null;
 
         if (!BCrypt.checkpw(password, user.getPassword())) {
             return null;
         }
+        return user;
+    }
     public UserService() {
         this.userDao = new UserInfoDao();
     }
 
-        return user;
+//
     public Optional<User> getUserProfileById(int id) {
         return userDao.findById(id);
     }
@@ -39,9 +41,9 @@ public class UserService {
     // Cập nhật mật khẩu
     public String updatePassword(int userId, String newPass) {
         String hashed = BCrypt.hashpw(newPass, BCrypt.gensalt());
-
-        boolean ok = userDao.updatePassword(userId, hashed);
+        boolean ok = userDAO.updatePassword(userId, hashed);
         return ok ? "Đổi mật khẩu thành công" : "Đổi mật khẩu thất bại";
+        }
     public Optional<User> getUserProfileByUsername(String username) {
         return userDao.findByUsername(username);
     }
